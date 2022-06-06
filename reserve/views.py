f6c050abbd40d222bb23170ce497173879f6725c
csepanyij <csepanyij@gmail.com>
2022-06-06 19:51:08 +0200

    typesetting views

--- reserve/views.py
+++ reserve/views.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Optional, Type
+from typing import Any, Dict, List, Optional, Type
 
 from django.conf import settings
 from django.shortcuts import render
@@ -11,6 +11,7 @@ from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
 from django.db.models import Model
+from django.db.models.query import QuerySet
 
 from .forms import SearchRestaurantForm, CreateRestaurantForm
 from .models import Restaurant
@@ -24,29 +25,29 @@ class GenericReservationView(View):
 
 
 class IndexView(GenericReservationView):
-    form_class = SearchRestaurantForm
-    template_name = 'reserve/search_restaurant.html'
+    form_class: Type[Model] = SearchRestaurantForm
+    template_name: str = 'reserve/search_restaurant.html'
 
-    def get(self, request: HttpRequest, *args, **kwargs):
+    def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
         form = self.form_class()
         return render(request, self.template_name, {'form': form})
 
 
 class RestaurantListView(GenericReservationView, ListView):
-    model = Restaurant
-    context_object_name = 'restaurants'
+    model: Type[Model] = Restaurant
+    context_object_name: str = 'restaurants'
 
-    def get_queryset(self):
-        self.form = SearchRestaurantForm(data=self.request.GET or None)
+    def get_queryset(self) -> QuerySet:
+        self.form: SearchRestaurantForm = SearchRestaurantForm(data=self.request.GET or None)
         form = self.form
         if self.request.GET and form.is_valid():
-            queryset = Restaurant.objects.filter(name__icontains=form.cleaned_data['name'])
+            queryset: QuerySet = Restaurant.objects.filter(name__icontains=form.cleaned_data['name'])
         else:
-            queryset = super().get_queryset()
+            queryset: QuerySet = super().get_queryset()
         return queryset
 
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
-        context = super().get_context_data(**kwargs)
+        context: Dict[str, Any] = super().get_context_data(**kwargs)
         context['form'] = self.form
         context['search_term'] = self.form.cleaned_data['name']
         return context
@@ -54,8 +55,8 @@ class RestaurantListView(GenericReservationView, ListView):
 
 class RestaurantCreateView(GenericReservationView, LoginRequiredMixin, CreateView):
     template_name: str = 'reserve/create_restaurant.html'
-    model = Restaurant
-    fields = ['name', 'address', 'rating', 'description', 'image']
+    model: Type[Model] = Restaurant
+    fields: List[str] = ['name', 'address', 'rating', 'description', 'image']
 
     def form_valid(self, form: Form) -> HttpResponse:
         form.instance.owned_by = self.request.user
@@ -70,7 +71,7 @@ class RestaurantDetailView(GenericReservationView, DetailView):
     model: Type[Model] = Restaurant
     context_object_name: Optional[str] = 'restaurant'
 
-    def get_queryset(self):
+    def get_queryset(self) -> QuerySet:
         print(self.kwargs['pk'])
         return Restaurant.objects.filter(pk=self.kwargs['pk'])
 
